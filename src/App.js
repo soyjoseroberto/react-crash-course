f52e5c5224a12f88c1e797507965d01172dc6e42
soyjoseroberto <jrodriguez18@gmail.com>
2023-01-21 14:39:17 -0500

    Created Message comp to learn useState hook.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 // for named export in Greet.js you need to object destructure
-import { Greet } from './components/Greet'
+import { Greet } from './components/Greet';
+import { Message } from './components/Message'
 
 function App() {
   // A component should always return 1 element (after return statement, have one wrapper element)
@@ -12,6 +13,7 @@ function App() {
       <Greet name='Diana' heroName='Wonder Woman'>
         <p>This is a children prop for Diana comp</p>
       </Greet>
+      <Message />
     </div>
   );
 }

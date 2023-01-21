150cbc05d1647fb2d3b4c0164f473dc7c07a7e44
soyjoseroberto <jrodriguez18@gmail.com>
2023-01-21 14:30:32 -0500

    Added a props.children to Wonder Woman comp.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     <div className="App">
       <h1>Hello Jose</h1>
       <Greet name='Bruce' heroName='Batman' />
+      <Greet name='Clark' heroName='Superman' />
+      <Greet name='Diana' heroName='Wonder Woman'>
+        <p>This is a children prop for Diana comp</p>
+      </Greet>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,13 @@
 export const Greet = (props) => {
     return (
-        <h1>{props.name} a.k.a {props.heroName}</h1>
+        <div>
+            <h1>{props.name} a.k.a {props.heroName}</h1>
+            <div>{props.children}</div>
+        </div>
     )
 }
 
 // export default Greet
+// Then you can import like: import Greet from 'Greet'; without {}
+
+// props.children returns whatever innerHTML is enclosed between the component tags
